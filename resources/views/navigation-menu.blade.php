236f2c5e0f3279fcfa2fea6be1756fb4f513d2f6
Braxton <Braxtonmassengale@icloud.com>
2022-12-05 15:12:24 +0100

    Added layout to newly created friends and create a review pages

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,10 +15,10 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Home') }}
                     </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-jet-nav-link href="{{ route('create') }}" :active="request()->routeIs('create')">
                         {{ __('Rate a Tortilla') }}
                     </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-jet-nav-link href="{{ route('friends') }}" :active="request()->routeIs('friends')">
                         {{ __('Friends List') }}
                     </x-jet-nav-link>
                 </div>
